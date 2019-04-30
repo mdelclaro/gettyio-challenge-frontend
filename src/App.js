d659a6fb197edf70a6490b53ad9c9da2b8983b44
Matheus Del Claro <matheusdelclaro@gmail.com>
2019-04-30 09:17:07 -0300

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import { BrowserRouter } from "react-router-dom";
+
+import Navbar from "./components/layout/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App" />
+      <div className="App">
+        <Navbar />
+      </div>
     </BrowserRouter>
   );
 }
