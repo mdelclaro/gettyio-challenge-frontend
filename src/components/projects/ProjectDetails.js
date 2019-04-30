0602036d09d97335c363617d6a813b77a3f1ba56
Matheus Del Claro <matheusdelclaro@gmail.com>
2019-04-30 16:44:30 -0300

    fixed project page

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import moment from "moment";
 
 import { getProject } from "../../store/actions";
 
@@ -12,28 +13,38 @@ class ProjectDetails extends Component {
     const { getProject, match } = this.props;
     const project = await getProject(match.params.id);
     this.setState({ project });
+    console.log(project);
   }
 
   render() {
     const id = this.props.match.params.id;
     const { project } = this.state;
-    return (
-      project && (
-        <div className="container section project-details">
-          <div className="card z-depth-0">
-            <div className="card-content">
-              <span className="card-title">
-                {project.title} - {id}
-              </span>
-              <p>{project.content}</p>
+    return project ? (
+      <div className="container section project-details">
+        <div className="card z-depth-0">
+          <div className="card-content">
+            <span className="card-title">
+              {project.title} - {id}
+            </span>
+            <p>{project.content}</p>
+          </div>
+          <div className="card-action grey lighten-4 grey-text">
+            <div>
+              Posted by {project.createdBy.firstName}{" "}
+              {project.createdBy.lastName}
             </div>
-            <div className="card-action grey lighten-4 grey-text">
-              <div>Posted by Someone</div>
-              <div>2nd September, 2am</div>
+            <div>
+              {moment(project.createdAt).format(
+                "dddd, MMMM Do YYYY. h:mm:ss a"
+              )}
             </div>
           </div>
         </div>
-      )
+      </div>
+    ) : (
+      <div className="container center">
+        <p>Loading project...</p>
+      </div>
     );
   }
 }
